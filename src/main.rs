1fef3eb119520d301f63aecc967e9d6857c3ca13
Adrian Rojo <rojoadrian16@gmail.com>
2023-12-27 19:23:16 -0500

    Taking user input using Druid

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,43 @@
-//  use std::io;
+// Include dependencies
+// use reqwest;
+use tokio;
+use dotenv::dotenv;
+//use std::io;
+// use serde::{Deserialize, Serialize};
 
-// use reqwest::blocking::get;
-    // Include dependencies
-    use reqwest;
-    use tokio;
-    use dotenv::dotenv;
-    use std::io;
-    use serde::{Deserialize, Serialize};
-    use druid::{Data, widget::{Label, Button, Flex}, Env, Widget, WindowDesc, AppLauncher};
-    
-//   fn main() {
-//     println!("enter your city");
-//     let mut city = String::new();
+//Druid is how we handle the GUI
+use druid::widget::prelude::*;
+use druid::widget::{Flex, Label, TextBox};
+use druid::{AppLauncher, Data, Lens, UnitPoint, WidgetExt, WindowDesc};
 
-//     io::stdin()
-//     .read_line(&mut city)
-//     .expect("Enter a word");
-    
-//     println!("Result: {city}");
+const VERTICAL_WIDGET_SPACING: f64 = 20.0;
+const TEXT_BOX_WIDTH: f64 = 200.0;
 
-    
-//     let base_url: &str = "http://api.weatherapi.com/v1/current.json";
-
-//     // let resq: Result<Response, Error> = get(base_url)?;
-
-//     // let resp: serde_json::Value = get(url)?.json()?;
-//     // println!("{:?}", resq);
-#[derive(Clone,Data)]
+#[derive(Clone,Data,Lens)]
 struct WeatherUi {
-    data: i32
+    // GUI
+    // data: i32,
+    city: String,
 }
 
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Location {
-    name: String,
-    region: String,
-    country: String,
-    //temp_f: String,
-    // feelslike_c: u32,
-    // feelslike_f: u32,
-    // cloud: u32,
-    // wind_mph: u32,
-    // humidity: i32,
-    // wind_dir: String,
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct Current {
-    temp_f: f32,
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct WeatherData {
-    location: Location,
-    current: Current,
-}
+// #[derive(Serialize, Deserialize, Debug)]
+// struct Location {
+//     // API - WeatherData.Location
+//     name: String,
+//     region: String,
+//     country: String,
+// }
+// #[derive(Serialize, Deserialize, Debug)]
+// struct Current {
+//     // API - WeatherData.Current
+//     temp_f: f32,
+// }
+// #[derive(Serialize, Deserialize, Debug)]
+// struct WeatherData {
+//     location: Location,
+//     current: Current,
+// }
 
 // API JSON response
 // {
@@ -66,69 +50,102 @@ struct WeatherData {
 //     }
 // }
 
-fn ui() -> impl Widget<WeatherUi> {
-    let label = Label::new(|data: &WeatherUi, _: &Env| format!("Counter: {}", data.data));
-    let increment = Button::new("+")
-        .on_click(|_ctx, data: &mut WeatherUi, _env| data.data += 1);
-    let decrement = Button::new("-")
-        .on_click(|_ctx, data: &mut WeatherUi, _env| data.data -= 1);
+// fn ui() -> impl Widget<WeatherUi> {
+//     let label = Label::new(|data: &WeatherUi, _: &Env| format!("Counter: {}", data.data));
+//     let increment = Button::new("+")
+//         .on_click(|_ctx, data: &mut WeatherUi, _env| data.data += 1);
+//     let decrement = Button::new("-")
+//         .on_click(|_ctx, data: &mut WeatherUi, _env| data.data -= 1);
 
-    Flex::column().with_child(label).with_child(Flex::row().with_child(increment).with_child(decrement))
-}
+//     Flex::column().with_child(label).with_child(Flex::row().with_child(increment).with_child(decrement))
+
+//     let txt_Box = TextBox::new()
+//         .with_placeholder("Who are we greeting?")
+//         .with_text_size(18.0)
+//         // .fix_width(TEXT_BOX_WIDTH)
+//         .lens(WeatherUi::name);
+// }
 
 // Tells the compiler to run this async function at runtime, in this case main.
 #[tokio::main]
     async fn main() -> Result<(), reqwest::Error> {
 
-        let main_window = WindowDesc::new(ui())
-        .title("test Window");
-        AppLauncher::with_window(main_window)
-        .log_to_console()
-        .launch(WeatherUi { data: 0 }).unwrap();
+
 
         // load .env variables
         dotenv().ok();
         // set our api key to a usuable variable
-        let api_key = std::env::var("API_KEY").expect("API Key is not set");
-        //println!("{}",api_key);
+        let api_key = std::env::var("API_KEY").expect("API key is not set");
+        print!("{}",api_key);
+        // Describe the window we are creating
+        let main_window = WindowDesc::new(build_root_widget())
+            .title("Weather app in rust")
+            .window_size((400.0,400.0));
 
-        println!("Enter your city");
-        let mut city = String::new();
+        // initial app state
+        let initial_state: WeatherUi = WeatherUi{
+            city: "test".into(),
+        };
+
+        // Launch the app
+        AppLauncher::with_window(main_window)
+            .log_to_console()
+            .launch(initial_state)
+            .expect("Oh no! Failed to launch");
+
+
+
+
+
+    // // The API endpoint
+    //     let url = format!("http://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no", api_key, city);
         
-        io::stdin()
-        .read_line(&mut city)
-        .expect("Enter a word");
-            
-        //     println!("Result: {city}");
-    // The API endpoint
-        let url = format!("http://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no", api_key, city);
-        
-    // Send a GET request to the url
-       let res = reqwest::get(url).await?;
+    // // Send a GET request to the url
+    //    let res = reqwest::get(url).await?;
 
-        // get the status code, should be 200
-        println!("Status: {}", res.status());
-        // println!("Headers:\n{:#?}", res.headers());
+    //     // get the status code, should be 200
+    //     println!("Status: {}", res.status());
+    //     // println!("Headers:\n{:#?}", res.headers());
 
-        // api response
-        let body = res.text().await?;
+    //     // api response
+    //     let body = res.text().await?;
 
-        let weather_data: WeatherData = serde_json::from_str(&body).expect("error");
+    //     let weather_data: WeatherData = serde_json::from_str(&body).expect("error");
 
-        println!("Body:\n{}", body);
+    //     println!("Body:\n{}", body);
 
-        println!("{:#?}", weather_data);
-        println!("{}", weather_data.current.temp_f);
-        //println!("Country: {}", weather_data.location.country);
-        //println!("F: {}",weather_data.location.feelslike_f);
-        //println!("humidity: {}", weather_data.location.humidity);
-        //println!("wind_mph: {}", weather_data.location.wind_mph);
+    //     println!("{:#?}", weather_data);
+    //     println!("{}", weather_data.current.temp_f);
+
         // this is successful return the result
         Ok(())
     }
 
   
-        
+    fn build_root_widget() -> impl Widget<WeatherUi> {
+        // a label that will determine its text based on the current app data.
+        let label = Label::new(|data: &WeatherUi, _env: &Env| {
+            if data.city.is_empty() {
+                "Hello anybody!?".to_string()
+            } else {
+                format!("Hello {}!", data.city)
+            }
+        })
+        .with_text_size(32.0);
+        // a textbox that modifies `name`.
+        let textbox = TextBox::new()
+            .with_placeholder("Who are we greeting?")
+            .with_text_size(18.0)
+            .fix_width(TEXT_BOX_WIDTH)
+            .lens(WeatherUi::city);
+
+    // arrange the two widgets vertically, with some padding
+        Flex::column()
+            .with_child(label)
+            .with_spacer(VERTICAL_WIDGET_SPACING)
+            .with_child(textbox)
+            .align_vertical(UnitPoint::CENTER)
+    }    
 
 
 
